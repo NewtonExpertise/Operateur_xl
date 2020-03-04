0800bf6964b85100a6e361dc80d3d17c7038063f
LeroyMathieu <48203163+LeroyMathieu@users.noreply.github.com>
2020-03-04 17:08:33 +0100

    First commit

---
+++ actions.py
@@ -0,0 +1,421 @@
+import os
+from mdbagent import MdbConnect
+import xlwings as xw
+from datetime import timedelta,datetime
+import re
+
+
+
+def ecritures_analytiques(mdbpath):
+    """
+    Renvoie vers le tableur la listes des écritures analytiques
+    """
+
+    sql = f"""
+    SELECT
+        E.CodeJournal AS Journal, 
+        DateSerial(Year(E.PeriodeEcriture), Month(E.PeriodeEcriture), E.JourEcriture) AS DateEcr,
+        E.NumeroCompte AS Compte, E.Libelle, E.MontantTenuDebit AS Debit, E.MontantTenuCredit AS Credit, 
+        (E.MontantTenuDebit-E.MontantTenuCredit) AS Solde,
+        E.NumeroPiece AS Piece, A.Centre, E.RefImage, E.CodeOperateur AS Oper, E.DateSysSaisie
+    FROM
+        (
+            SELECT 
+                TypeLigne, NumUniq, NumeroCompte, CodeJournal,  Folio, LigneFolio, 
+                PeriodeEcriture, JourEcriture, NumLigne, Libelle, MontantTenuDebit, MontantTenuCredit, 
+                NumeroPiece, CodeOperateur, DateSysSaisie, RefImage 
+            FROM Ecritures 
+            WHERE TypeLigne='E' 
+            AND (NumeroCompte LIKE '6%' OR NumeroCompte LIKE '7%')) E
+    LEFT JOIN
+        (
+            SELECT 
+                TypeLigne, CodeJournal, Folio, LigneFolio, PeriodeEcriture, JourEcriture, NumLigne, Centre 
+            FROM Ecritures WHERE TypeLigne='A') A
+    ON E.CodeJournal=A.CodeJournal
+    AND E.Folio=A.Folio
+    AND E.LigneFolio=A.LigneFolio
+    AND E.PeriodeEcriture=A.PeriodeEcriture
+    """
+    # Récupération data
+    with MdbConnect(mdbpath) as mdb:
+        info, data = mdb.queryInfoData(sql)
+    headers = [x[0] for x in info]
+    data.insert(0, headers)
+
+    # Création nouvelle onglet
+    ws = xw.sheets.active
+    wb = ws.book
+    num_client = os.path.basename(os.path.dirname(mdbpath))
+    base = os.path.basename(os.path.dirname(os.path.dirname(mdbpath)))
+    Nom_feuille_excel = "EC_Analytique_"+num_client+"_"+base
+    add_sheet_new_name(wb, Nom_feuille_excel)
+
+    # Création des plages
+    bnligne=len(data)
+    if base == "DC":
+        xw.Range('I1:I'+str(bnligne)).name = 'ColCentreN'
+        xw.Range('A1:A'+str(bnligne)).name = 'ColJournauxN'
+        xw.Range('G1:G'+str(bnligne)).name = 'ColSoldeN'
+    else:
+        xw.Range('I1:I'+str(bnligne)).name = 'ColCentreN1'
+        xw.Range('A1:A'+str(bnligne)).name = 'ColJournauxN1'
+        xw.Range('G1:G'+str(bnligne)).name = 'ColSoldeN1'
+
+    # formatage
+    xw.Range('H:H').number_format='@'
+    xw.Range('C:C').number_format='@'
+    xw.Range('E:G').number_format='# ##0,00'
+    xw.Range('A1').value = data
+    ws.autofit()
+    # return data
+
+def ecritures(mdbpath):
+    """
+    Renvoie vers le tableur la listes des écritures 
+    """
+    sql = """
+    SELECT
+    DateSerial(Year(E.PeriodeEcriture), Month(E.PeriodeEcriture), E.JourEcriture) as DateEcr,
+    E.NumeroCompte, E.CodeJournal, E.Libelle, E.MontantTenuDebit,
+    E.MontantTenuCredit, E.NumeroPiece, E.CodeOperateur, E.DateSysSaisie, A.Centre, T.DateEcheance, E.RefImage
+    FROM
+    (
+        (SELECT TypeLigne, NumUniq, NumeroCompte, CodeJournal,  Folio, LigneFolio, PeriodeEcriture, JourEcriture, NumLigne, Libelle, MontantTenuDebit, MontantTenuCredit, NumeroPiece, CodeOperateur, DateSysSaisie, RefImage FROM Ecritures WHERE TypeLigne='E') E
+    LEFT JOIN
+        (SELECT TypeLigne, CodeJournal, Folio, LigneFolio, PeriodeEcriture, JourEcriture, NumLigne, Centre FROM Ecritures WHERE TypeLigne='A') A
+    ON E.CodeJournal=A.CodeJournal
+    AND E.Folio=A.Folio
+    AND E.LigneFolio=A.LigneFolio
+    AND E.PeriodeEcriture=A.PeriodeEcriture)
+    LEFT JOIN
+    (SELECT TypeLigne, CodeJournal, Folio, LigneFolio, PeriodeEcriture, JourEcriture, NumLigne, DateEcheance FROM Ecritures WHERE TypeLigne='T') T
+    ON E.CodeJournal=T.CodeJournal
+    AND E.Folio=T.Folio
+    AND E.LigneFolio=T.LigneFolio
+    AND E.PeriodeEcriture=T.PeriodeEcriture
+    """
+    with MdbConnect(mdbpath) as mdb:
+        info, data = mdb.queryInfoData(sql)
+    headers = [x[0] for x in info]
+    data.insert(0, headers)
+
+    ws = xw.sheets.active
+    wb = ws.book
+    num_client = os.path.basename(os.path.dirname(mdbpath))
+    base = os.path.basename(os.path.dirname(os.path.dirname(mdbpath)))
+    Nom_feuille_excel = "Ecritures_"+num_client+"_"+base
+    add_sheet_new_name(wb, Nom_feuille_excel)
+
+    xw.Range('G:G').number_format='@'
+    xw.Range('B:B').number_format='@'
+    xw.Range('E:F').number_format='# ##0,00'
+    xw.Range('A1').value = data
+    ws.autofit()
+
+def balance_generale_totale(mdbpath, fin_periode):
+    """
+    Renvoie vers le tableur la balance générale
+    """    
+    sql = f"""
+    SELECT E.NumeroCompte, C.Intitule,
+    SUM(E.MontantTenuDebit) AS Debit, SUM(E.MontantTenuCredit) AS Credit,
+    (Debit - Credit) AS Solde
+    FROM Ecritures E
+    LEFT JOIN Comptes C
+    ON E.NumeroCompte=C.Numero
+    WHERE E.PeriodeEcriture < #{fin_periode}#
+    AND E.TypeLigne = 'E'
+    GROUP BY E.NumeroCompte, C.Intitule
+    ORDER BY E.NumeroCompte
+    """
+    with MdbConnect(mdbpath) as mdb:
+        info, data = mdb.queryInfoData(sql)
+    headers = [x[0] for x in info]
+    data.insert(0, headers)
+
+    ws = xw.sheets.active
+    wb = ws.book
+    num_client = os.path.basename(os.path.dirname(mdbpath))
+    base = os.path.basename(os.path.dirname(os.path.dirname(mdbpath)))
+    Nom_feuille_excel = "B_Generale_totale_"+num_client+"_"+base
+    add_sheet_new_name(wb, Nom_feuille_excel)
+    
+    xw.Range('C:C').number_format='# ##0,00'
+    xw.Range('D:D').number_format='# ##0,00'
+    xw.Range('E:E').number_format='# ##0,00'
+    xw.Range('A:A').number_format='@'
+    xw.Range('A1').value = data
+    ws.autofit()
+
+def balance_generale(mdbpath, fin_periode):
+    """
+    Renvoie vers le tableur la balance simplifié
+    """
+    prefix_auxiliaire = get_auxiliaire_prefix(mdbpath) 
+    # Contrôle présence auxiliaire : 
+    # S'il n'y a pas de paramétrage des auxiliaires :
+    if prefix_auxiliaire['client']== None:
+        balance_generale_totale(mdbpath, fin_periode)
+    # Sinon :
+    else:
+        sql = f"""
+        SELECT E.NumeroCompte AS Numero_Compte, C.Intitule AS Libelle,
+        SUM(E.MontantTenuDebit) AS Debit, SUM(E.MontantTenuCredit) AS Credit,
+        (Debit - Credit) AS Solde
+        FROM Ecritures E
+        LEFT JOIN Comptes C
+        ON E.NumeroCompte=C.Numero
+        WHERE C.Type = 'G'
+        AND E.TypeLigne = 'E'
+        AND E.PeriodeEcriture < #{fin_periode}#
+        GROUP BY E.NumeroCompte, C.Intitule
+        ORDER BY E.NumeroCompte
+        UNION
+        SELECT '41100000' AS Numero_Compte, 'Clients' AS Libelle,
+        SUM(E.MontantTenuDebit) AS Debit, SUM(E.MontantTenuCredit) AS Credit,
+        (Debit - Credit) AS Solde
+        FROM Ecritures E
+        LEFT JOIN Comptes C
+        ON E.NumeroCompte=C.Numero
+        WHERE E.NumeroCompte LIKE '{prefix_auxiliaire['client']}%'
+        AND E.TypeLigne = 'E'
+        AND E.PeriodeEcriture < #{fin_periode}#
+        UNION
+        SELECT '40100000' AS Numero_Compte, 'Fournisseurs' AS Libelle,
+        SUM(E.MontantTenuDebit) AS Debit, SUM(E.MontantTenuCredit) AS Credit,
+        (Debit - Credit) AS Solde
+        FROM Ecritures E
+        LEFT JOIN Comptes C
+        ON E.NumeroCompte=C.Numero
+        WHERE E.NumeroCompte LIKE '{prefix_auxiliaire['fournisseur']}%'
+        AND E.TypeLigne = 'E'
+        AND E.PeriodeEcriture < #{fin_periode}#
+        """
+        with MdbConnect(mdbpath) as mdb:
+            info, data = mdb.queryInfoData(sql)
+        headers = [x[0] for x in info]
+        data.insert(0, headers)
+
+        ws = xw.sheets.active
+        wb = ws.book
+        num_client = os.path.basename(os.path.dirname(mdbpath))
+        base = os.path.basename(os.path.dirname(os.path.dirname(mdbpath)))
+        Nom_feuille_excel = "B_Generale_"+num_client+"_"+base
+        add_sheet_new_name(wb, Nom_feuille_excel)
+        
+        xw.Range('C:C').number_format='# ##0,00'
+        xw.Range('D:D').number_format='# ##0,00'
+        xw.Range('E:E').number_format='# ##0,00'
+        xw.Range('A:A').number_format='@'
+        xw.Range('A1').value = data
+        ws.autofit()
+
+def balance_clients(mdbpath, fin_periode):
+    """
+    Renvoie vers le tableur la balance clients
+    """
+    prefix_auxiliaire = get_auxiliaire_prefix(mdbpath)
+    sql = f"""
+    SELECT E.NumeroCompte, C.Intitule,
+    SUM(E.MontantTenuDebit) AS Debit, SUM(E.MontantTenuCredit) AS Credit,
+    (Debit - Credit) AS Solde
+    FROM Ecritures E
+    LEFT JOIN Comptes C
+    ON E.NumeroCompte=C.Numero
+    WHERE C.Type = 'C'
+    AND E.NumeroCompte LIKE '{prefix_auxiliaire['client']}%'
+    AND E.TypeLigne = 'E'
+    AND E.PeriodeEcriture < #{fin_periode}#
+    GROUP BY E.NumeroCompte, C.Intitule
+    ORDER BY E.NumeroCompte
+    """
+    with MdbConnect(mdbpath) as mdb:
+        info, data = mdb.queryInfoData(sql)
+    headers = [x[0] for x in info]
+    data.insert(0, headers)
+
+    ws = xw.sheets.active
+    wb = ws.book
+    num_client = os.path.basename(os.path.dirname(mdbpath))
+    base = os.path.basename(os.path.dirname(os.path.dirname(mdbpath)))
+    Nom_feuille_excel = "B_clients_"+num_client+"_"+base
+    add_sheet_new_name(wb, Nom_feuille_excel)
+
+    xw.Range('C:C').number_format='# ##0,00'
+    xw.Range('D:D').number_format='# ##0,00'
+    xw.Range('E:E').number_format='# ##0,00'
+    xw.Range('A:A').number_format='@'
+    xw.Range('A1').value = data
+    ws.autofit()
+
+def balance_fournisseurs(mdbpath, fin_periode):
+    """
+    Renvoie vers le tableur la balance fournisseurs
+    """
+    prefix_auxiliaire = get_auxiliaire_prefix(mdbpath)
+    sql = f"""
+    SELECT E.NumeroCompte, C.Intitule,
+    SUM(E.MontantTenuDebit) AS Debit, SUM(E.MontantTenuCredit) AS Credit,
+    (Debit - Credit) AS Solde
+    FROM Ecritures E
+    LEFT JOIN Comptes C
+    ON E.NumeroCompte=C.Numero
+    WHERE C.Type = 'F'
+    AND E.NumeroCompte LIKE '{prefix_auxiliaire['fournisseur']}%'
+    AND E.TypeLigne = 'E'
+    AND E.PeriodeEcriture < #{fin_periode}#
+    GROUP BY E.NumeroCompte, C.Intitule
+    ORDER BY E.NumeroCompte
+    """
+    with MdbConnect(mdbpath) as mdb:
+        info, data = mdb.queryInfoData(sql)
+    headers = [x[0] for x in info]
+    data.insert(0, headers)
+
+    ws = xw.sheets.active
+    wb = ws.book
+    num_client = os.path.basename(os.path.dirname(mdbpath))
+    base = os.path.basename(os.path.dirname(os.path.dirname(mdbpath)))
+    Nom_feuille_excel = "B_fournisseurs_"+num_client+"_"+base
+    add_sheet_new_name(wb, Nom_feuille_excel)
+
+    xw.Range('C:C').number_format='# ##0,00'
+    xw.Range('D:D').number_format='# ##0,00'
+    xw.Range('E:E').number_format='# ##0,00'
+    xw.Range('A:A').number_format='@'
+    xw.Range('A1').value = data
+    ws.autofit()
+
+def codes_journaux(mdbpath):
+    sql="""
+    SELECT Code from Journaux ORDER BY Code;
+    """
+    with MdbConnect(mdbpath) as mdb:
+        data = mdb.query(sql)
+    set1 = {x[0] for x in data}
+
+    # Requête sur la base des paramètres généraux QcomptaC
+    drive, _ = os.path.splitdrive(mdbpath)
+    QcomptaC = os.path.abspath(os.path.join(drive, "quadra/database/cpta/qcomptac.mdb"))
+    with MdbConnect(QcomptaC) as mdb:
+        data = mdb.query(sql)
+    set2 = {x[0] for x in data}
+
+    fullset = sorted(set1.union(set2))
+
+    row, col = callSelectedCell()
+
+    for i, value in enumerate(fullset, row):
+        xw.Range((i, col)).value = value
+
+def callSelectedCell():
+    """
+    Retourne les coordonnées de la cellule sélectionnée
+    """
+    wb = xw.books.active  
+    selected_row = wb.app.selection.row
+    selected_col = wb.app.selection.column
+    return selected_row, selected_col
+
+def get_mois_exercice(QcomptaC):
+    """
+    Renvoie la listes des mois de l'exercice.
+    """
+    sql = """
+    SELECT DebutExercice, FinExercice, DateLimiteSaisie
+    FROM Dossier1
+    """
+    with MdbConnect(QcomptaC) as mdb:
+        periode = mdb.query(sql)
+    
+    debut = periode[0][0] # Début d'exercice
+    fin = periode[0][1] # Fin d'exercice
+    date_limite = periode[0][2] # Date limite en cas d'exercice multiple sur le DC
+    no_date_limite = datetime(year = 1899, month = 12, day = 30) #Valeur renvoyé par Access lorsqu'un champ date est vide.
+    test_liste = []
+    # # Contrôle s'il y a une date limite de set alors on la prend en compte pour retourner la période total.
+    if date_limite == no_date_limite:
+        while debut <= fin :
+            
+            test_liste.append(end_of_month(debut))
+            debut = end_of_month(debut)+ timedelta(1)
+    # Sinon on utilise la date de fin d'exercice pour établir la période.
+    else:
+        date_limite= end_of_month(date_limite)
+        while debut <= date_limite :
+            test_liste.append(end_of_month(debut))
+            debut = end_of_month(debut)+ timedelta(1)
+ 
+    return test_liste
+
+def get_auxiliaire_prefix(QcomptaC):
+    """
+    Récupère le préfixe des codes clients-fournisseurs :
+    """
+    sql = """
+    SELECT
+    CodifClasse0Seule
+    FROM Dossier2
+    """
+    with MdbConnect(QcomptaC) as mdb:
+        data = mdb.query(sql)
+    rt = {}
+    if data[0][0] == "F":
+        rt['fournisseur']=0
+        rt['client']=9
+    elif data[0][0] == "C":
+        rt['fournisseur']=9
+        rt['client']=0
+    else:
+        rt['fournisseur']=None
+        rt['client']=None
+
+    return rt
+
+def end_of_month(dt0):
+    """
+    Renvoi le dernier jour du mois de la date donnée
+    """
+    dt1 = dt0.replace(day=1)
+    dt2 = dt1 + timedelta(days=32)
+    dt3 = dt2.replace(day=1) - timedelta(days=1)
+    return dt3
+
+def add_sheet_new_name(wb, nom):
+    """
+    Génère une feuille excel avec un nom unique
+    nb : une feuille excel ne peut contenir que 31 caractères
+    """
+    nom = nom[:29]
+    increment = 0
+    sheet = [sheet.name for sheet in wb.sheets]
+    if nom in sheet:
+
+        for sheet_name in sheet:
+            try:
+                name, compteur , _ =  re.split(nom+r'(\d+)',sheet_name) 
+            except ValueError:
+                compteur =0
+            if increment< int(compteur):
+                increment = int(compteur)
+
+        new_name = nom + str(increment+1)
+    else:
+        new_name = nom 
+    new_ws = wb.sheets.add(new_name)
+    new_ws.book.activate(True)
+
+    return new_ws
+
+
+if __name__ == "__main__":
+    # import pprint
+    # pp=pprint.PrettyPrinter(indent=4)
+    mdb=  r'\\srvquadra\Qappli\Quadra\DATABASE\cpta\DA2018\000737\qcompta.mdb'
+    # pp.pprint(get_mois_exercice(mdb))
+    # import xlwings as xw
+    # ws = xw.sheets.active
+    # wb=ws.book
+    ecritures_analytiques(mdb)
+
---
+++ main.py
@@ -0,0 +1,168 @@
+from tkinter import *
+from tkinter import messagebox
+from tkinter.ttk import Combobox
+import actions
+from quadraenv import QuadraSetEnv
+from espion import update_espion
+from datetime import datetime
+from time import strftime, strptime
+import locale
+import os
+locale.setlocale(locale.LC_TIME,'')
+
+class Application(Frame):
+
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.couleur = "#E4AB5B"
+        self.pack()
+        self.create_widgets()
+
+    def create_widgets(self):
+
+        # Création des widgets
+        self.var_dossiers = StringVar()
+        self.var_dossiers.set("")
+        self.lab_dossier = Label(self, text="Dossiers \ud83d\udd0e",font=('Helvetica', 12) , foreground='orange')
+        self.lab_select_dossier = Label(self, text="\ud83d\udc49",font=('Helvetica', 14, 'bold'),foreground='orange')
+        self.lab_base = Label(self, text="\ud83d\udc49",font=('Helvetica', 14, 'bold'),foreground='orange')
+        self.lab_select_action = Label(self, text="\ud83d\udc49",font=('Helvetica', 14,'bold'),foreground='orange')
+        self.lab_Fin_Periode = Label(self, text="\ud83d\udcc6",font=('Helvetica', 14, 'bold') , foreground='orange')
+        self.saisie1 = Entry(self, width=25, textvariable=self.var_dossiers, cursor='question_arrow')
+        self.saisie1.focus_set()
+        self.liste_dossiers = Listbox(self, width=25, height=8, selectbackground=self.couleur, cursor="hand2")
+        self.liste_bases = Listbox(self, width=25, height=5, selectbackground=self.couleur, cursor="hand2")
+        self.liste_bases.config(height=0)
+        self.liste_actions = Listbox(self,width=25, selectbackground=self.couleur, cursor="hand2")
+        self.liste_actions.config(height=0)
+        self.combobox_periode = Combobox(self, width=22, state="readonly", cursor="hand2")
+
+        # Positions
+        self.lab_dossier.grid(row=0, column=0, padx=10, pady=3,sticky='e')
+        self.saisie1.grid(row=0, column=1, padx=10, pady=3)
+        self.liste_dossiers.grid(row=1, column=1, padx=10, pady=3)
+        self.lab_select_dossier.grid(row=1, column=0, padx=10, pady=3,sticky='e')
+        self.liste_bases.grid(row=2, column=1, padx=10, pady=3)
+        self.liste_actions.grid(row=3, column=1, padx=10, pady=3)
+
+        # Actions Binding
+        self.liste_dossiers.bind("<ButtonRelease-1>", self.makeListeBase)
+        self.liste_bases.bind("<ButtonRelease-1>", self.setMdbPath)
+        self.liste_actions.bind("<ButtonRelease-1>", self.setAction)
+        self.combobox_periode.bind("<<ComboboxSelected>>", self.setAction_periode)
+        
+        # Callback pour filtrage de la liste dossiers
+        self.var_dossiers.trace("w", lambda name, index,
+                                mode: self.filter_list_dossier())
+
+        # Dictionnaires des actions
+        self.dispatch = {
+            actions.ecritures.__name__: actions.ecritures,
+            actions.ecritures_analytiques.__name__: actions.ecritures_analytiques,
+            actions.codes_journaux.__name__: actions.codes_journaux,
+            actions.balance_generale_totale.__name__: actions.balance_generale_totale,
+            actions.balance_generale.__name__: actions.balance_generale,
+            actions.balance_clients.__name__: actions.balance_clients,
+            actions.balance_fournisseurs.__name__: actions.balance_fournisseurs
+        }
+        for i, action in enumerate(self.dispatch.keys()):
+            self.liste_actions.insert(i, action)
+
+        self.liste_actions.configure(state=DISABLED)
+        self.makeListeDossier()
+        self.filter_list_dossier()
+
+    def setMdbPath(self, e):
+        """
+        Définit le chemin complet vers la base qcompta (mdb)
+        """
+        self.lab_Fin_Periode.grid_forget()
+        self.combobox_periode.grid_forget()
+        self.lab_select_action.grid(row=3, column=0, padx=10, pady=3,sticky='e')
+        self.liste_actions.configure(state=NORMAL)
+        index, = self.liste_bases.curselection()
+        self.base = self.liste_bases.get(index)
+        for base, chemin in self.dbList:
+            if self.base == base:
+                self.mdb = chemin
+
+
+    def makeListeDossier(self):
+        """
+        Prépare le liste des dossiers
+        """
+        self.dicDossier = {}
+        ipl = r"\\srvquadra\qappli\quadra\database\client\quadra.ipl"
+        self.qenv = QuadraSetEnv(ipl)
+        for code, rs in self.qenv.gi_list_clients():
+            label = f"{rs} ({code})"
+            self.dicDossier.update({label: code})
+
+    def makeListeBase(self, e):
+        """
+        Prépare la liste des bases (DC, DA, ...)
+        """
+        self.liste_actions.configure(state=DISABLED)
+        self.lab_select_action.grid_forget()
+        self.combobox_periode.grid_forget()
+        self.lab_Fin_Periode.grid_forget()
+        self.lab_base.grid(row=2, column=0, padx=10, pady=3,sticky='e')
+        self.liste_bases.delete(0, END)
+        index, = self.liste_dossiers.curselection()
+        value = self.liste_dossiers.get(index)
+        self.code_dossier = self.dicDossier[value]
+        self.dbList = self.qenv.recent_cpta(self.code_dossier, depth=3)
+        for i, (base, _) in enumerate(self.dbList):
+            self.liste_bases.insert(i, base)
+
+    def filter_list_dossier(self):
+        """
+        Filtrage auto de la liste des dossiers
+        """
+        search_term = self.var_dossiers.get()
+        lbox_list = [x for x in self.dicDossier.keys()]
+        self.liste_dossiers.delete(0, END)
+        for item in lbox_list:
+            if search_term.lower() in item.lower():
+                self.liste_dossiers.insert(END, item)
+
+    def setAction(self, e):
+        """
+        Sélection du programmes qui sera lancé
+        """
+        # self.liste_actions.configure(state='disabled')
+        index, = self.liste_actions.curselection()
+        value = self.liste_actions.get(index)
+        if "balance" in value:
+            self.combobox_periode.grid(row = 5, column = 1, padx = 10, pady = 3)
+            self.lab_Fin_Periode.grid(row=4, column=0, rowspan= 4, padx=10, pady=3,sticky='e')
+            index, = self.liste_actions.curselection()
+            self.select_action = self.liste_actions.get(index)
+            periode = [ date.strftime("%Y-%B") for date in actions.get_mois_exercice(self.mdb)]
+            self.combobox_periode['values'] = periode
+        else:
+            self.dispatch[value](self.mdb)
+            messagebox.showinfo("Annonce", "Export terminé")
+            update_espion(self.code_dossier, self.base, f"operateur;{value}")
+            sys.exit()
+    
+    def setAction_periode(self, e):
+        """
+        Sélection du programmes qui sera lancé avec une période choisie
+        """
+        # mois sélectionné :
+        select_mois = self.combobox_periode.get()
+        select_mois = actions.end_of_month(datetime.strptime(select_mois, "%Y-%B"))
+        self.dispatch[self.select_action](self.mdb, select_mois)
+        messagebox.showinfo("Annonce", "Export terminé")
+        update_espion(self.code_dossier, self.base, f"operateur;{self.select_action}")
+        sys.exit()
+
+
+root = Tk()
+root.title('Opérateur Excel')
+root.wm_attributes("-topmost", 1)
+ressources = os.path.dirname(sys.argv[0])
+root.iconbitmap(os.path.join(ressources,"IMG/favicon.png"))
+app = Application(master=root)
+app.mainloop()
